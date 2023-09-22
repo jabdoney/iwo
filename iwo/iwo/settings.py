4338caf58cf2dc6ab22b61825b2cb2e5e175afad
kabdoney <kabdoney@jud10.flcourts.org>
2023-09-22 11:27:37 -0400

    Sept22 Recommit

--- iwo/iwo/settings.py
+++ iwo/iwo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 if not DEBUG:
